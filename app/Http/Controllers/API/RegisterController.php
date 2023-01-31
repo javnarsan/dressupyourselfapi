59f45ffd95e3d396acbc1095dff54e656d4bb582
Javier Naranjo <javnarsan@gmail.com>
2023-01-31 22:05:22 +0100

    Login

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -31,4 +31,20 @@ class RegisterController extends Controller {
 
         return response()->json(['success' => $success], $this->successStatus);
     }
+
+    public function login() {
+        // Si las credenciales son correctas
+        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+            $user = Auth::user();
+            // Creamos un token de acceso para ese usuario
+            $success['token'] = $user->createToken('MyApp')->accessToken;
+
+            // Y lo devolvemos en el objeto 'json'
+            return response()->json(['success' => $success], $this->successStatus);
+        }
+        else {
+            return response()->json(['error' => 'No estás autorizado'], 401);
+        }
+    }
+
 }
